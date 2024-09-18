ce36a1136a80fcbbe91dd968255df6c3a0e8886e
diary45 <42435491+diary45@users.noreply.github.com>
2024-09-18 18:44:04 +0530

    initial commit

--- tournamentClassification.py
+++ tournamentClassification.py
@@ -1,27 +1,30 @@
 import io
 import sys
-
 import chess.pgn
 import time
+import numpy as np
 
 
-def pgn_to_numpy_multiple_games(pgn_file):
-    all_games_states = []
+def tournament_counter(pgn_file):
+    all_games_states = np.array([])
     chessgamedata = sys.stdin.read()
     reader=io.StringIO(chessgamedata)
-    all_games_states=[]
     tournamentcount=0
     start = time.time()
-
+    is_present = lambda x : "tournament" in x.headers.get("Event")
+    vec_is_present = np.vectorize(is_present)
     while True:
         game = chess.pgn.read_game(reader)
         if game is None:
             break
         if "tournament" in game.headers.get("Event"):
             tournamentcount=tournamentcount+1
+        np.append(all_games_states,game)
     end = time.time()
-    print("time taken is ",end-start)
+    print(" time taken is ",end-start)
     print(tournamentcount)
+    start = time.time()
+
 
 pgn_file = "/Users/vishnudvardhan/Downloads/lichess_db_antichess_rated_2024-08.pgn"
-pgn_to_numpy_multiple_games(pgn_file)
+tournament_counter(pgn_file)

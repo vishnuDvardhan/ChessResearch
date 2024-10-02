9786d6efae676ab13b388ce3eab52cb15e954174
Vishnu Vardhan <vishnu@vedan.iith.ac.in>
2024-10-02 15:21:23 +0530

    parallelised code added

--- tournamentClassification.py
+++ tournamentClassification.py
@@ -7,23 +7,26 @@ import numpy as np
 
 tournamentcount = 0
 lock = threading.Lock()
+lock1 = threading.Lock()
 chessgamedata = sys.stdin.read()
 reader=io.StringIO(chessgamedata)
 
 def tournament_counter():
     global tournamentcount
-    with lock:
-        while True:
-            game = chess.pgn.read_game(reader)
-            if game is None:
-                break
-            if "tournament" in game.headers.get("Event"):
-                tournamentcount=tournamentcount+1
-            print(game.headers.get("Event"),tournamentcount)
+    global reader
+    lock.acquire()
+    game = chess.pgn.read_game(reader)
+    lock.release()
+    while True:
+        if game is None:
+            break
+        if "tournament" in game.headers.get("Event"):
+            tournamentcount=tournamentcount+1
+        print(game.headers.get("Event"),tournamentcount)
 
 
 threadPool=[]
-num_threads=16
+num_threads=4
 start = time.time()
 
 for i in range(num_threads):

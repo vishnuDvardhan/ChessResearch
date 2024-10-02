35bbac73a4ed8b6b027f1ca753cde96b573e3d3d
diary45 <42435491+diary45@users.noreply.github.com>
2024-10-02 23:27:25 +0530

    reading through string stream in pgn file

--- tournamentClassification.py
+++ tournamentClassification.py
@@ -1,25 +1,18 @@
 import io
 import sys
-import chess.pgn
+import re
 import time
 import numpy as np
 
 
 def tournament_counter():
-    all_games_states = np.array([])
-    chessgamedata = sys.stdin.read()
-    reader=io.StringIO(chessgamedata)
     tournamentcount=0
     start = time.time()
-    while True:
-        game = chess.pgn.read_game(reader)
-        if game is None:
-            break
-        if "tournament" in game.headers.get("Event"):
+    pattern = re.compile(r'^\[Event\s+".*tournament.*"\]$')
+    for line in sys.stdin:
+        if (pattern.match(line)):
             tournamentcount=tournamentcount+1
-            print(game.headers.get("Event"),tournamentcount)
-
-        np.append(all_games_states,game)
+            print(line , tournamentcount)
     end = time.time()
     print(" time taken is ",end-start)
     print(tournamentcount)

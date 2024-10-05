9e627e8eb0c2f570177a7c9fdc1e29f3b10c5a6b
vishnuDvardhan <vishnudvardhan1@gmail.com>
2024-10-05 07:33:32 +0530

    implementing counter in c++

---
+++ toutnamentClassification.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <map>
+#include <chrono>
+
+using namespace std;
+
+void tournament_counter() {
+    map<string, int> counts = {
+        {"Blitz", 0}, {"Bullet", 0}, {"Rapid", 0}, {"UltraBullet", 0}, {"Classical", 0}
+    };
+    int tournament_count = 0;
+    auto start = chrono::high_resolution_clock::now();
+    regex pattern1(R"(^\[Event\s+".*tournament.*"\]$)");
+    regex pattern2(R"((\w+)\s+tournament)");
+
+    string line;
+    while (getline(cin, line)) {
+        if (regex_match(line, pattern1)) {
+            tournament_count++;
+            cout << "Total tournaments: " << tournament_count << "\n";
+
+            smatch match;
+            if (regex_search(line, match, pattern2)) {
+                string type = match[1].str();
+                counts[type]++;
+            }
+        }
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Time taken is: " << elapsed.count() << " seconds\n";
+    cout << "Tournament counts:\n";
+    for (const auto& pair : counts) {
+        cout << pair.first << ": " << pair.second << "\n";
+    }
+    cout << "Total tournaments: " << tournament_count << "\n";
+}
+
+int main() {
+    tournament_counter();
+    return 0;
+}
